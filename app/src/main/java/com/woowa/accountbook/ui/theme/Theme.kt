b929cbb4a1b29ddd301bd568ea4eb9ec111542f1
Zzu-h <juho7785@naver.com>
2022-07-28 13:35:59 +0900

    ui: Color Palette 추가 및 배경 색상 변경
    - Color 추가 및 배경 색상 오프화이트로 변경

--- app/src/main/java/com/woowa/accountbook/ui/theme/Theme.kt
+++ app/src/main/java/com/woowa/accountbook/ui/theme/Theme.kt
@@ -9,13 +9,15 @@ import androidx.compose.runtime.Composable
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = BackgroundDark
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple700,
     primaryVariant = Purple500,
-    secondary = Teal200
+    secondary = Teal200,
+    background = OffWhite
 
     /* Other default colors to override
     background = Color.White,
