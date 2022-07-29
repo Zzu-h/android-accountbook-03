714903ac689e6f2ee51b0c180fcbd5f8bf099b13
Zzu-h <juho7785@naver.com>
2022-07-29 20:55:19 +0900

    feature: 각 메뉴 프래그먼트 생성
    - 각 메뉴에 해당하는 프래그먼트 생성

--- app/src/main/java/com/woowa/accountbook/ui/MainActivity.kt
+++ app/src/main/java/com/woowa/accountbook/ui/MainActivity.kt
@@ -7,7 +7,10 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.woowa.accountbook.R
 import com.woowa.accountbook.databinding.ActivityMainBinding
+import com.woowa.accountbook.ui.calendar.CalendarFragment
 import com.woowa.accountbook.ui.history.HistoryFragment
+import com.woowa.accountbook.ui.setting.SettingFragment
+import com.woowa.accountbook.ui.statistic.StatisticFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -36,6 +39,21 @@ class MainActivity : AppCompatActivity() {
                 R.id.item_history -> changeFragment(
                     HistoryFragment(),
                     getString(R.string.fragment_history),
+                    false
+                )
+                R.id.item_calendar -> changeFragment(
+                    CalendarFragment(),
+                    getString(R.string.fragment_calendar),
+                    false
+                )
+                R.id.item_statistic -> changeFragment(
+                    StatisticFragment(),
+                    getString(R.string.fragment_statistic),
+                    false
+                )
+                R.id.item_setting -> changeFragment(
+                    SettingFragment(),
+                    getString(R.string.fragment_setting),
                     false
                 )
             }
