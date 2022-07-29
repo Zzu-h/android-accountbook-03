02113a713d52cceed44396dba64257e2ebd920c6
Zzu-h <juho7785@naver.com>
2022-07-30 03:39:48 +0900

    feat: 캘린더 팝업창 띄우기
    - 공통으로 사용될 캘린더 팝업창 생성

---
+++ app/src/main/java/com/woowa/accountbook/ui/common/popup/MonthYearPickerDialog.kt
@@ -0,0 +1,57 @@
+package com.woowa.accountbook.ui.common.popup
+
+import android.app.AlertDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.View
+import android.widget.NumberPicker
+import androidx.fragment.app.DialogFragment
+import com.woowa.accountbook.R
+import com.woowa.accountbook.databinding.DatePickerDialogBinding
+
+class MonthYearPickerDialog(private val year: Int, private val month: Int) : DialogFragment() {
+
+    lateinit var binding: DatePickerDialogBinding
+
+    private var listener: OnDateSetListener? = null
+    fun setListener(listener: OnDateSetListener?) {
+        this.listener = listener
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(activity)
+        val inflater = requireActivity().layoutInflater
+
+        val dialog: View = inflater.inflate(R.layout.date_picker_dialog, null)
+        val monthPicker = dialog.findViewById<NumberPicker>(R.id.picker_month)
+        val yearPicker = dialog.findViewById<NumberPicker>(R.id.picker_year)
+
+        monthPicker.minValue = 0
+        monthPicker.maxValue = 11
+        monthPicker.value = month
+
+        yearPicker.minValue = year
+        yearPicker.maxValue = MAX_YEAR
+        yearPicker.value = year
+
+        builder.setView(dialog)
+            .setPositiveButton(R.string.ok,
+                DialogInterface.OnClickListener { dialog, id ->
+                    listener!!.onDateSet(
+                        null,
+                        yearPicker.value,
+                        monthPicker.value,
+                        0
+                    )
+                })
+            .setNegativeButton(R.string.cancel,
+                DialogInterface.OnClickListener { dialog, id -> this@MonthYearPickerDialog.dialog!!.cancel() })
+        return builder.create()
+    }
+
+    companion object {
+        private const val MAX_YEAR = 2099
+    }
+}
